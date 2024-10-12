cb7479aa1cc21b3988adf3da48f612525e0da7e6
KrishnaSriR <KrishnaSriR@users.noreply.github.com>
2024-10-11 23:57:17 -0500

    transaction layout changes

--- app/src/main/java/com/android/exchangeproproject/TransactionActivity.java
+++ app/src/main/java/com/android/exchangeproproject/TransactionActivity.java
@@ -1,5 +1,6 @@
 package com.android.exchangeproproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
@@ -27,6 +28,14 @@ public class TransactionActivity extends AppCompatActivity {
         payButton.setOnClickListener(view -> {
             // Payment logic
             Toast.makeText(TransactionActivity.this, "Payment Processed", Toast.LENGTH_SHORT).show();
+        });
+        Button backToHomeButton = findViewById(R.id.back_to_home_button);
+        backToHomeButton.setOnClickListener(view -> {
+            // Navigate back to HomeActivity
+            Intent intent = new Intent(TransactionActivity.this, HomeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
         });
     }
 }
